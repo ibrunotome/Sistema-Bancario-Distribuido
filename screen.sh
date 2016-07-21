4aaae4f3eea9f415c24ab295bc27aafa74e33f5f
Bruno Tomé <ibrunotome@gmail.com>
2016-07-21 10:17:38 -0300

    .sh updated

--- screen.sh
+++ screen.sh
@@ -6,4 +6,4 @@ cd ./src/
 export CLASSPATH=$CLASSPATH:../jgroups-3.6.4.Final.jar
 find . -name "*.java" -print | xargs javac
 
-java views.UserScreen
+java views.UserScreen 2> /dev/null
--- server.sh
+++ server.sh
@@ -6,4 +6,4 @@ cd ./src/
 export CLASSPATH=$CLASSPATH:../jgroups-3.6.4.Final.jar
 find . -name "*.java" -print | xargs javac
 
-java controllers.ServerBank
+java controllers.ServerBank 2> /dev/null
